4f52ca2a27f15f33bd64cb305c830b6d26ef7c44
Nithins mac <bsnithin44@gmail.com>
2019-10-03 01:35:26 +0530

    update added uploading and saving image feature

--- mysite/editor/urls.py
+++ mysite/editor/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'editor'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',views.editor,name='editor')
 ]
--- mysite/editor/views.py
+++ mysite/editor/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 
 
@@ -9,3 +9,19 @@ def index(request):
 def home(request):
     return render(request = request,
                 template_name = 'editor/home.html')
+
+def editor(request):
+    if request.method == "POST":
+        # a = 1
+        # print the uploded file
+        uploadedFile = request.FILES['document']
+        fs = FileSystemStorage()
+        image_name = fs.save(uploadedFile.name,uploadedFile)
+        image_url = fs.url(image_name)
+        data = {}
+        data['image_url'] = image_url
+        print(data)
+        return render(request,'editor/editor.html',data)
+    else:
+        # Ask to upload the file
+        return render(request,'editor/editor.html')
